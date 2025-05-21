78d51d29663fb2c368bcc0257153d1f7cdd01879
lucas mendes <lucasmndes.dev@gmail.com>
2025-05-21 13:24:30 -0300

    adicionando cors

--- src/server.js
+++ src/server.js
@@ -2,8 +2,16 @@ import express from 'express';
 
 import routes from './routes.js';
 
+import cors from 'cors'
+
 const app = express();
 
+
+app.use(cors({
+    origin: 'http://localhost:3000',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    credentials: true
+}))
 
 app.use(express.json());
 
