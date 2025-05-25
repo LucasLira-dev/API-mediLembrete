c0c56cac5c56e197b88edc0b3f7396ea6f958eb3
lucas mendes <lucasmndes.dev@gmail.com>
2025-05-25 13:19:06 -0300

    pequeno ajuste

--- src/app/repositories/UsuariosRepository.js
+++ src/app/repositories/UsuariosRepository.js
@@ -1,9 +1,18 @@
 import { consulta } from "./../database/conexao.js";
 
 class UsuariosRepository {
-    create(email, senha) {
+    async create(email, senha) {
+        const existe = await consulta(
+            'SELECT * FROM usuarios WHERE email = ?',
+            [email],
+            'Erro ao buscar usuário!'
+        ); //verifica se o email já existe
+        if (existe.length > 0) {
+            throw new Error('Email já cadastrado!');
+        }
+
         const sql = `INSERT INTO usuarios (email, senha) VALUES (?, ?)`;
-        return consulta(sql, [email, senha], 'Não foi possivel cadastrar o usuário!')
+        return consulta(sql, [email, senha], 'Não foi possivel cadastrar o usuário!'); // insere o usuário se não existir
     }
 }
 
