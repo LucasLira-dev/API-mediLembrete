5f5e94362d6ace0c234f08e4c9225e686e9de690
lucas mendes <lucasmndes.dev@gmail.com>
2025-05-27 11:18:30 -0300

    criptografando a senha

--- src/app/controllers/UsuariosController.js
+++ src/app/controllers/UsuariosController.js
@@ -1,14 +1,19 @@
 import UsuariosRepository from '../repositories/UsuariosRepository.js';
+import bcrypt from 'bcryptjs';
 
 class UsuariosController {
     async store(req, res) {
         const { email, senha } = req.body;
         try {
-            const resultado = await UsuariosRepository.create(email, senha);
+            const senhaCriptografada = await bcrypt.hash(senha, 10); // Criptografa a senha
+            const resultado = await UsuariosRepository.create(email, senhaCriptografada);
             res.status(201).json({
                 userId: resultado.insertId
             });
         } catch (error) {
+            if (error.message === 'Email já cadastrado!') {
+                return res.status(400).json({ error: 'Email já cadastrado' });
+            }
             res.status(500).json({ error: error.message || 'Erro ao cadastrar usuário' });
         }
     }
