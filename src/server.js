5fdc49296b07f46d7d20746fed0ccc79d54cb318
lucas mendes <lucasmndes.dev@gmail.com>
2025-05-29 15:50:43 -0300

    tentando corrigir o erro

--- src/server.js
+++ src/server.js
@@ -20,7 +20,7 @@ app.use(express.json());
 // usando o Router
 app.use(routes)
 
-const PORT = process.env.DB_PORT || 3000;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
