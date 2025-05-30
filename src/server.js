b813c9501bef00ba964537d8403876d4d3902dba
lucas mendes <lucasmndes.dev@gmail.com>
2025-05-30 09:34:47 -0300

    teste

--- src/server.js
+++ src/server.js
@@ -12,13 +12,7 @@ const app = express();
 console.log('FRONTEND_URL:', process.env.FRONTEND_URL);
 
 app.use(cors({
-    origin: function (origin, callback) {
-        if (!origin || origin === process.env.FRONTEND_URL) {
-            callback(null, true);
-        } else {
-            callback(new Error('Not allowed by CORS'));
-        }
-    },
+    origin: 'http://localhost:3000',
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     credentials: true
 }));
